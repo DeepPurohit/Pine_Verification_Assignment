0d0f96cc6c445f207c596086a2a44ce55256ac3e
Deep Purohit <DeepPurohit@users.noreply.github.com>
2018-09-03 11:36:35 +0530

    code updated

--- Verif_assign/sequence_detector.c
+++ Verif_assign/sequence_detector.c
@@ -5,15 +5,15 @@ void main(){
     system("clear");
     printf("Enter the sequence\n");
     printf("\n*Input should be '1' or '0'* \n");   
-        for(i= 0; i<10; i++){
+       /* for(i= 0; i<10; i++){
             scanf("%d",&in[i]);
             if(in[i]>1 && in[i] == 01){
                 printf("\n**wrong input**\n");
                 exit(0);
             }
                 
-        }
-printf("\nOutput\n");
+        }*/
+/*printf("\nOutput\n");
 printf("----------\n");
     while(j<10){
         switch(state){
@@ -76,4 +76,62 @@ printf("----------\n");
 
     }
     printf("\n");
+}*/
+    do{ 
+        scanf("%d",&in);   
+        printf("\nOutput\n");
+        printf("----------\n");
+                switch(state){
+                    case 0:
+                            if(in == 1){
+                                printf("0");
+                                printf("\nGoing to S1\n");
+                                state = 1;
+                            }
+                            else{
+                                printf("0");
+                                printf("\nGoing to S0\n");
+                                state = 0;
+                            }
+                            break;
+                    case 1:
+                            if(in == 0){
+                                printf("0");
+                                printf("\nGoing to S2\n");
+                                state = 2;
+                            }
+                            else{
+                                printf("0");
+                                printf("\nGoing to S1\n");
+                                state = 1;
+                            }
+                            break;
+                    case 2:
+                            if(in == 1){
+                                printf("0");
+                                printf("\nGoing to S3\n");
+                                state = 3;
+                            }
+                            else{
+                                printf("0");
+                                printf("\nGoing to S0\n");
+                                state = 0;
+                            }
+                            break;
+                    case 3:
+                            if(in == 1){
+                                printf("1");
+                                printf("\nGoing to S0\n");
+                                state = 0;
+                            }
+                            else{
+                                printf("0");
+                                printf("\nGoing to S2\n");
+                                state = 2;
+                            }
+            }
+        printf("want to continue?(y/n)\t");
+        scanf("%c",&ch);
+    }while(ch=='y');
+            printf("\n");
 }
